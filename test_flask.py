0c5cad34cfc336d83c3bd3c5247fba504d19f3d4
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-02 12:38:20 -0700

    got the unit test to work for test_user_page

--- test_flask.py
+++ test_flask.py
@@ -1,7 +1,7 @@
 
 from unittest import TestCase
 from app import app
-from models import db, User
+from models import db, User, Post, Tag, PostTag
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
@@ -20,15 +20,23 @@ class UserViewsTestCase(TestCase):
     
     def setUp(self):
         """Clean up any existing Users in the db."""
-        
+        Post.query.delete()
         User.query.delete()
         
+        
         user = User(first_name='Frank', last_name='Ocean', img_url='https://a.espncdn.com/photo/2015/1212/r35307_1296x729_16-9.jpg')
-
+        # tag = Tag(name='Acting')
         db.session.add(user)
+        # db.session.add(tag)
+        db.session.commit()
+        
+        post = Post(title='My home in como.', content='Its pretty dope.', user=user)
+
+        db.session.add(post)
         db.session.commit()
         
         self.user_id = user.id
+        self.post_id = post.id
         self.client = app.test_client()
     
     def tearDown(self):
@@ -37,15 +45,15 @@ class UserViewsTestCase(TestCase):
         db.session.rollback()
         
         
-    def test_list_users(self):
-        """ Making sure that the home page renders a list of users in db. """
+    # def test_list_users(self):
+    #     """ Making sure that the home page renders a list of users in db. """
 
-        with self.client as client:
-            res = self.client.get("/")
-            html = res.get_data(as_text=True)
+    #     with self.client as client:
+    #         res = self.client.get("/")
+    #         html = res.get_data(as_text=True)
 
-            self.assertEqual(res.status_code, 200)
-            self.assertIn('Frank Ocean', html) 
+    #         self.assertEqual(res.status_code, 200)
+    #         self.assertIn('Frank Ocean', html) 
             
     def test_user_page(self):
         """ Making sure that the user page renders correct html. """
@@ -55,7 +63,8 @@ class UserViewsTestCase(TestCase):
             html = res.get_data(as_text=True)
 
             self.assertEqual(res.status_code, 200)
-            self.assertIn('<p class="user_name">Frank Ocean</p>', html)    
+            self.assertIn('<p class="user_name">Frank Ocean</p>', html) 
+             
             
     
     def test_add_user(self):
